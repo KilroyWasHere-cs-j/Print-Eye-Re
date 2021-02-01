715ef85dc2092638db99bb9f56b790cb5b82f437
KilroyWasHere-cs-j <59028838+KilroyWasHere-cs-j@users.noreply.github.com>
2021-02-01 10:20:27 -0500

    Add files via upload
    Setup.exe and Start.exe and a lot of hope.

---
+++ Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+// Build path C:\Users\gmtow\source\repos\PrintEye\PrintEye\bin\Debug
+namespace PrintEye
+{
+    public partial class Form1 : Form
+    {
+        public readonly string filePath = "main.py";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            Process.Start("C:\\Users\\gmtow\\source\\repos\\PrintEye\\PrintEye\\bin\\Debug\\main.py");
+        }
+    }
+}
